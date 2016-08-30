a27823db5340350f9121403f3ffee3cc7bf663af
Sergey Suprunenko <suprunenko.s@gmail.com>
2016-08-30 20:01:46 +0300

    Initial Phoenix commit

---
+++ config/config.exs
@@ -0,0 +1,23 @@
+# This file is responsible for configuring your application
+# and its dependencies with the aid of the Mix.Config module.
+#
+# This configuration file is loaded before any dependency and
+# is restricted to this project.
+use Mix.Config
+
+# Configures the endpoint
+config :web, Web.Endpoint,
+  url: [host: "localhost"],
+  secret_key_base: "uoDGC1em9ttxDb+xU333+DQku9WUYGde9SlBi6foakFvXKb7YzT+sMQ1PhIvVOTt",
+  render_errors: [view: Web.ErrorView, accepts: ~w(html json)],
+  pubsub: [name: Web.PubSub,
+           adapter: Phoenix.PubSub.PG2]
+
+# Configures Elixir's Logger
+config :logger, :console,
+  format: "$time $metadata[$level] $message\n",
+  metadata: [:request_id]
+
+# Import environment specific config. This must remain at the bottom
+# of this file so it overrides the configuration defined above.
+import_config "#{Mix.env}.exs"
---
+++ web/views/layout_view.ex
@@ -0,0 +1,3 @@
+defmodule Web.LayoutView do
+  use Web.Web, :view
+end
