4e237b566965d0574f6a3d1233d85ec4fd925a31
Sergey Suprunenko <suprunenko.s@gmail.com>
2016-09-02 17:50:41 +0300

    Add two static pages

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -4,4 +4,12 @@ defmodule Web.PageController do
   def index(conn, _params) do
     render conn, "index.html"
   end
+
+  def sample(conn, _params) do
+    render conn, "sample.html"
+  end
+
+  def empty(conn, _params) do
+    render conn, "empty.html"
+  end
 end
--- web/router.ex
+++ web/router.ex
@@ -17,6 +17,8 @@ defmodule Web.Router do
     pipe_through :browser # Use the default browser stack
 
     get "/", PageController, :index
+    get "/sample", PageController, :sample
+    get "/empty", PageController, :empty
   end
 
   # Other scopes may use custom stacks.
