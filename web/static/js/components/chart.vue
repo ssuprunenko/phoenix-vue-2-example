04b351e3028ead69e6f30840d33777756adec838
Sergey Suprunenko <suprunenko.s@gmail.com>
2016-09-02 17:51:24 +0300

    Use props for chart component

--- web/static/js/components/chart.vue
+++ web/static/js/components/chart.vue
@@ -15,8 +15,8 @@ export default {
       chartData: {
         labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
         datasets: [{
-          label: '# of Votes',
-          data: [12, 19, 3, 5, 2, 3],
+          label: this.label,
+          data: this.values,
           backgroundColor: [
             'rgba(255, 99, 132, 0.2)',
             'rgba(54, 162, 235, 0.2)',
@@ -35,6 +35,19 @@ export default {
           ],
           borderWidth: 1
         }]
+      }
+    }
+  },
+
+  props: {
+    label: {
+      type: String,
+      default: 'Unknown'
+    },
+    values: {
+      type: Array,
+      default () {
+        return []
       }
     }
   },
