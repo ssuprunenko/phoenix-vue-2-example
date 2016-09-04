2af4058d41754241d42ee66c014aa99c2f3d74df
Sergey Suprunenko <suprunenko.s@gmail.com>
2016-09-04 17:47:14 +0300

    Rename components to capitalize-2

---
+++ web/static/js/components/Chart.vue
@@ -0,0 +1,87 @@
+<template>
+  <div>
+    <button v-on:click='redrawChart'>Redraw!</button>
+    <canvas id='myChart' width='700' height='300'></canvas>
+  </div>
+</template>
+
+<script>
+import Chart from 'chart.js'
+
+export default {
+  data () {
+    return {
+      myChart: null,
+      chartData: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+          label: this.label,
+          data: this.values,
+          backgroundColor: [
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(255, 206, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(153, 102, 255, 0.2)',
+            'rgba(255, 159, 64, 0.2)'
+          ],
+          borderColor: [
+            'rgba(255,99,132,1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)',
+            'rgba(153, 102, 255, 1)',
+            'rgba(255, 159, 64, 1)'
+          ],
+          borderWidth: 1
+        }]
+      }
+    }
+  },
+
+  props: {
+    label: {
+      type: String,
+      default: 'Unknown'
+    },
+    values: {
+      type: Array,
+      default () {
+        return []
+      }
+    }
+  },
+
+  mounted () {
+    this.drawChart()
+  },
+
+  methods: {
+    drawChart () {
+      const ctx = document.getElementById('myChart')
+      this.myChart = new Chart(ctx, {
+        type: 'bar',
+        data: this.chartData,
+        options: {
+          scales: {
+            yAxes: [{
+              ticks: {
+                beginAtZero: true
+              }
+            }]
+          }
+        }
+      })
+    },
+    redrawChart () {
+      this.$http.get('random').then((response) => {
+        this.chartData.datasets[0].data = response.json()
+        this.myChart.update()
+      })
+    }
+  }
+}
+</script>
+
+<style lang='css'>
+</style>
---
+++ web/static/js/components/Help.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class='col-lg-6'>
+    <h4>Help</h4>
+    <ul>
+      <li v-for='link in links'>
+        <a :href='link.url'>{{link.title | upcase}}</a>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      links: [
+        {
+          url: 'http://groups.google.com/group/phoenix-talk',
+          title: 'Mailing list'
+        },
+        {
+          url: 'http://webchat.freenode.net/?channels=elixir-lang',
+          title: '#elixir-lang on freenode IRC'
+        },
+        {
+          url: 'https://twitter.com/elixirphoenix',
+          title: '@elixirphoenix'
+        }
+      ]
+    }
+  },
+  filters: {
+    upcase (value) {
+      return value.toUpperCase()
+    }
+  }
+}
+</script>
+
+<style lang='css'>
+</style>
