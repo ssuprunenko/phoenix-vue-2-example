0cf8db2c1feaa29d31f0eec67fd914ce0253bd3c
Sergey Suprunenko <suprunenko.s@gmail.com>
2016-09-04 17:31:18 +0300

    Chat backend: copy-paste the code from phoenix guides

---
+++ web/channels/room_channel.ex
@@ -0,0 +1,20 @@
+defmodule Web.RoomChannel do
+	use Phoenix.Channel
+
+	def join("room:lobby", _message, socket) do
+		{:ok, socket}
+	end
+	def join("room:" <> _private_room_id, _params, _socket) do
+		{:error, %{reason: "unauthorized"}}
+	end
+
+	def handle_in("new_msg", %{"body" => body}, socket) do
+		broadcast! socket, "new_msg", %{body: body}
+		{:noreply, socket}
+	end
+
+	def handle_out("new_msg", payload, socket) do
+		push socket, "new_msg", payload
+		{:noreply, socket}
+	end
+end
