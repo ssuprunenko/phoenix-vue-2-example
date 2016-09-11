9856158ea03570000ebbbb68856b1f91573b2433
Sergey Suprunenko <suprunenko.s@gmail.com>
2016-09-11 16:49:07 +0300

    Create Message component

---
+++ web/static/js/components/Message.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="media message">
+    <div class="media-left">
+      <a href="#">
+        <img :src="msg.avatarUrl" class="avatar">
+      </a>
+    </div>
+    <div class="media-body">
+      <h5 class="media-heading">
+        {{msg.name}}
+        <small>{{msg.date}}</small>
+      </h5>
+      <em v-if="msg.isSystem" class="text-muted">{{msg.text}}</em>
+      <p v-else>{{msg.text}}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: ['msg']
+  }
+</script>
+
+<style>
+  .message .avatar {
+    width: 40px;
+    height: 40px;
+    border-radius: 10%;
+  }
+
+  .message .media-body {
+    overflow: visible;
+  }
+
+  .message p {
+    margin: 0;
+  }
+</style>
