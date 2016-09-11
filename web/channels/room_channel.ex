970d92800746f140b4d75e208b498360e56abe41
Sergey Suprunenko <suprunenko.s@gmail.com>
2016-09-11 16:49:54 +0300

    Add support for joining messages

--- web/channels/room_channel.ex
+++ web/channels/room_channel.ex
@@ -8,8 +8,8 @@ defmodule Web.RoomChannel do
 		{:error, %{reason: "unauthorized"}}
 	end
 
-	def handle_in("new_msg", %{"name" => name, "body" => body}, socket) do
-		broadcast! socket, "new_msg", %{name: name, body: body}
+	def handle_in("new_msg", %{"name" => name, "body" => body, "isSystem" => is_system}, socket) do
+		broadcast! socket, "new_msg", %{name: name, body: body, is_system: is_system}
 		{:noreply, socket}
 	end
 
--- web/static/js/components/MessageList.vue
+++ web/static/js/components/MessageList.vue
@@ -1,38 +1,24 @@
 <template>
   <div>
-    <div class='form-group'>
+    <div class="form-group">
       <input
-        v-model='currentMessage'
-        @keyup.enter='pushMessage'
-        type='text'
-        class='form-control'
-        placeholder='Message #random'
+        v-model="currentMessage"
+        @keyup.enter="pushMessage"
+        type="text"
+        class="form-control"
+        placeholder="Message #random"
       />
     </div>
 
-    <div>
-      <div class='media' v-for='msg in messages'>
-        <div class="media-left">
-          <a href="#">
-            <img :src="msg.avatarUrl" class="avatar">
-          </a>
-        </div>
-        <div class="media-body">
-          <h5 class='media-heading'>
-            {{msg.name}}
-            <small>{{msg.date}}</small>
-          </h5>
-          {{msg.text}}
-        </div>
-      </div>
-    </div>
+    <message v-for="msg in messages" :msg="msg" />
   </div>
 </template>
 
 <script>
+  import Message from './Message.vue'
+
   import socket from '../socket'
   const channel = socket.channel('room:lobby', {})
-  channel.join()
 
   export default {
     data () {
@@ -48,26 +34,36 @@
           name: payload.name,
           text: payload.body,
           date: new Date().toLocaleTimeString('en-US', { hour12: false }),
-          avatarUrl: `https://api.adorable.io/avatars/40/${payload.name}.png`
+          avatarUrl: `https://api.adorable.io/avatars/40/${payload.name}.png`,
+          isSystem: payload.is_system
         })
       })
+
+      this.joinChannel()
     },
     methods: {
       pushMessage () {
         channel.push('new_msg', {
           name: this.name,
-          body: this.currentMessage
+          body: this.currentMessage,
+          isSystem: false
         })
         this.currentMessage = ''
+      },
+      joinChannel () {
+        channel.join()
+          .receive('ok', resp => {
+            // Send message about joining the chat
+            channel.push('new_msg', {
+              name: this.name,
+              body: 'joined #random',
+              isSystem: true
+            })
+          })
       }
+    },
+    components: {
+      Message
     }
   }
 </script>
-
-<style>
-  .avatar {
-    width: 40px;
-    height: 40px;
-    border-radius: 10%;
-  }
-</style>
